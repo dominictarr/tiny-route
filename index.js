b342194acbf362ba12fc39297bbdea189b459474
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-14 16:58:32 +0100

    implement string routes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-
+function error(req, res) {
+  return function () {
+    res.end('no route for:' + req.url)
+  }
+}
 var route = module.exports = function (rx, handler, _next) {
+  rx = 'string' === typeof rx ? new RegExp('^'+rx) : rx
   return function (req, res, next) {
     var m = rx.exec(req.url)
-    if(!m) return (next || _next)()
+    if(!m) return (next || _next || error(req, res))()
     req.url = req.url.substring(m[0].length) || '/'
       
     req.params = [].slice.call(m, 1)
